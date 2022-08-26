8db73f018c6ceb28f1dee2390715274b496f14e7
David <73829106+EL-BANANITO@users.noreply.github.com>
2022-08-26 19:52:28 +0400

    code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import keyboard
 import time
 
 black = (0, 0, 0)
-white = (255, 255, 255)
+white = (255, 255, 255)#RGB colors
 red = (255, 0, 0)
 
 WIDTH = 10
@@ -15,7 +15,7 @@ for row in range(cube_count):
     grid.append([])
     for column in range(cube_count):
         grid[row].append(0)
-grid[1][5] = 0
+grid[1][5] = 0#1 draw cube 0 delete or not draw the cube
 pygame.init()
 window_size = [793, 793]
 scr = pygame.display.set_mode(window_size)
@@ -25,7 +25,7 @@ clock = pygame.time.Clock()
 cords = []  # list of all possible coordinates
 for i in range(72):
     for j in range(72):
-        cords.append([i, j])
+        cords.append([i, j])#get all possible coordinates of cubes
 coordinates = []
 x_cord = []
 y_cord = []
@@ -60,17 +60,15 @@ while not done:
     clock.tick(50)
     pygame.display.flip()
 
-    if keyboard.is_pressed('space'):
+    if keyboard.is_pressed('space'):#after space will be pressed game will start
         #time.sleep(0.01)
         index = []
-        for i in coordinates:
+        for i in coordinates:#get all live cubes coordinate indexes of list cords
             index.append(cords.index(i))
 
-        for j in index:
+        for j in index:#spawn(draw) cubes
             near_count = 0
             if j - 1 in index:
-                #grid[cords[j][0]][cords[j][1] + 1] = 1
-                #coordinates.append([cords[j][0], cords[j][1] + 1])
                 near_count += 1
             if j + 1 in index:
                 near_count += 1
@@ -86,19 +84,17 @@ while not done:
                 near_count += 1
             if j + 73 in index:
                 near_count += 1
-            if near_count != 2 and near_count != 3:
+            if near_count != 2 and near_count != 3:#if cube don't have 2 or 3 neighborhood cube will die(delete)
                 grid[cords[j][0]][cords[j][1]] = 0
                 coordinates.remove([cords[j][0], cords[j][1]])
 
         index_all = []
-        for i in cords:
+        for i in cords:#get all indexes of coordinate in list cords
             index_all.append(cords.index(i))
 
-        for j in index_all:
+        for j in index_all:#killing(erase) cubes
             near_count = 0
             if j - 1 in index:
-                # grid[cords[j][0]][cords[j][1] + 1] = 1
-                # coordinates.append([cords[j][0], cords[j][1] + 1])
                 near_count += 1
             if j + 1 in index:
                 near_count += 1
@@ -114,13 +110,13 @@ while not done:
                 near_count += 1
             if j + 73 in index:
                 near_count += 1
-            if near_count == 3:
+            if near_count == 3:#if an empty cube has 3 neighborhoods, then a new cube will be drawn
                 grid[cords[j][0]][cords[j][1]] = 1
                 coordinates.append([cords[j][0], cords[j][1]])
                 x_cord.append(cords[j][1])
                 y_cord.append(cords[j][0])
 
-    if keyboard.is_pressed('D'):
+    if keyboard.is_pressed('D'):#after key D will be pressed all cubes will be deleted
         coordinates = []
         for i in x_cord:
             for j in y_cord:
